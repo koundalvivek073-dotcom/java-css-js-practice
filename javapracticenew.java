936b2cde82986b258e609239b2cb3abef1916a8e
Vivek Koundal <koundalvivek073@gmail.com>
2026-09-21 15:54:10 +0530

    practiced java

---
+++ javapracticenew.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class javapracticenew{
+   public static void main(String[] var0) {
+    Scanner sc = new Scanner(System.in);
+    int arr_length = sc.nextInt();
+    int input [] = new int[arr_length];
+    int num = 9;
+    for(int i =0; i<arr_length; i++){
+      input[i] = sc.nextInt();
+      }
+    for(int i = 0;i<arr_length; i++){
+      if(input[0] + input[i] && input[i] != input[0]){
+         System.out.ptintln("0 "+ i);
+         else{
+            
+         }
+      }
+
+    }
+   }
+}
